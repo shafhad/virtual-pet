ae55ec63d0d28131afe8d39f645347e9bb045da2
shafhad <66770462+shafhad@users.noreply.github.com>
2020-12-23 12:38:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ var foodStock
 function preload()
 {
   //load images here
-  dog=loadImage("images/dogImg.png")
-  happyDog=loadImage("images/dogImg1.png")
+  dog=loadImage("dogImg.png")
+  happyDog=loadImage("dogImg1.png")
 }
 
 function setup() {
